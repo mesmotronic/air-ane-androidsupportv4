2d8b0bdff066ecc98bfb72fd6727b0126524bb08
Neil Rackett <neil.rackett@mesmotronic.com>
2020-07-06 16:46:43 +0100

    Added support for Android-ARM64

--- build.cmd
+++ build.cmd
@@ -5,5 +5,6 @@ adt ^
  -target ane AndroidSupportV4.ane extension.xml ^
  -swc swc/bin/air-ane-androidsupportv4-swc.swc ^
  -platform Android-ARM -C android . ^
+ -platform Android-ARM64 -C android . ^
  -platform Android-x86 -C android . ^
  -platform default -C default . 
